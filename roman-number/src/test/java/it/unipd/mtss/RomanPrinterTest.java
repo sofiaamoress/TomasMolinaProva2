cc21e7b2fccbb9ed37fdab0f22a524af876324d1
sofiaamoress <160346321+sofiaamoress@users.noreply.github.com>
2025-05-05 19:47:19 +0200

    #13 scambio definizione string RomanPrinterTest

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -5,25 +5,183 @@
 
 package it.unipd.mtss;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RomanPrinterTest {
-    String letteraI = " _____  \n|_   _| \n  | |   \n  | |   \n _| |_  \n|_____|\n";
-    String letteraV = "__      __\n\\ \\    / /\n \\ \\  / / \n  \\ \\/ /  \n   \\  /   \n    \\/    \n";
-    String letteraX = "__   __\n\\ \\ / /\n \\ V /  \n  > <   \n / · \\  \n/_/ \\_\\";
-    String letteraL = " _       \n| |      \n| |      \n| |      \n| |_ _   \n|______| ";
-    String letteraC = " _____  \n/  ___| \n| |     \n| |     \n| |___  \n\\_____| ";
-    String letteraD = " _____   \n|  __ \\  \n| |  | | \n| |  | | \n| |__| | \n|_____/  ";
-    String letteraM = " __  __  \n|  \\/  | \n| \\  / | \n| |\\/| | \n| |  | | \n|_|  |_| ";
-    String letteraXLIV = "__   __    _          _____    __   __\n\\ \\ / /   | |        |_   _|   \\ \\ / /   \n \\ V /    | |          | |      \\ V /    \n  > <     | |          | |       > <     \n / · \\    | |_ _      _| |_     / · \\    \n/_/ \\_\\   |______|   |_____|   /_/ \\_\\   ";
-    String letteraXCIV = "__   __    _____     _____    __      __\n\\ \\ / /   /  ___|   |_   _|   \\ \\    / /\n \\ V /    | |         | |      \\ \\  / /  \n  > <     | |         | |       \\ \\/ /    \n / · \\    | |___     _| |_       \\  /     \n/_/ \\_\\   \\_____|   |_____|       \\/      ";
-    String letteraCXXIII = " _____    __   __   __   __    _____     _____     _____\n/  ___|   \\ \\ / /   \\ \\ / /   |_   _|   |_   _|   |_   _|  \n| |        \\ V /     \\ V /      | |       | |       | |     \n| |         > <       > <       | |       | |       | |     \n| |___     / · \\     / · \\     _| |_     _| |_     _| |_    \n\\_____|   /_/ \\_\\   /_/ \\_\\   |_____|   |_____|   |_____|   ";
-    String letteraCCLIX = " _____     _____     _          _____    __   __ \n/  ___|   /  ___|   | |        |_   _|   \\ \\ / /   \n| |       | |       | |          | |      \\ V /    \n| |       | |       | |          | |       > <     \n| |___    | |___    | |_ _      _| |_     / · \\    \n\\_____|   \\_____|   |______|   |_____|   /_/ \\_\\   ";
-    String letteraCDLXVIII = " _____     _____      _         __   __   __      __   _____     _____     _____ \n/  ___|   |  __ \\    | |        \\ \\ / /   \\ \\    / /  |_   _|   |_   _|   |_   _|   \n| |       | |  | |   | |         \\ V /     \\ \\  / /     | |       | |       | |     \n| |       | |  | |   | |          > <       \\ \\/ /      | |       | |       | |     \n| |___    | |__| |   | |_ _      / · \\       \\  /      _| |_     _| |_     _| |_    \n\\_____|   |_____/    |______|   /_/ \\_\\       \\/      |_____|   |_____|   |_____|   ";
-    String letteraDLXXXVII = " _____      _         __   __   __   __   __   __   __      __   _____     _____ \n|  __ \\    | |        \\ \\ / /   \\ \\ / /   \\ \\ / /   \\ \\    / /  |_   _|   |_   _|   \n| |  | |   | |         \\ V /     \\ V /     \\ V /     \\ \\  / /     | |       | |     \n| |  | |   | |          > <       > <       > <       \\ \\/ /      | |       | |     \n| |__| |   | |_ _      / · \\     / · \\     / · \\       \\  /      _| |_     _| |_    \n|_____/    |______|   /_/ \\_\\   /_/ \\_\\   /_/ \\_\\       \\/      |_____|   |_____|   ";
-    String letteraDCCXXXVI = " _____      _____     _____    __   __   __   __   __   __   __      __   _____ \n|  __ \\    /  ___|   /  ___|   \\ \\ / /   \\ \\ / /   \\ \\ / /   \\ \\    / /  |_   _|   \n| |  | |   | |       | |        \\ V /     \\ V /     \\ V /     \\ \\  / /     | |   \n| |  | |   | |       | |         > <       > <       > <       \\ \\/ /      | |     \n| |__| |   | |___    | |___     / · \\     / · \\     / · \\       \\  /      _| |_    \n|_____/    \\_____|   \\_____|   /_/ \\_\\   /_/ \\_\\   /_/ \\_\\       \\/      |_____|   ";
-    String letteraCMLIV = " _____     __  __     _          _____    __      __ \n/  ___|   |  \\/  |   | |        |_   _|   \\ \\    / /  \n| |       | \\  / |   | |          | |      \\ \\  / /   \n| |       | |\\/| |   | |          | |       \\ \\/ /    \n| |___    | |  | |   | |_ _      _| |_       \\  /     \n\\_____|   |_|  |_|   |______|   |_____|       \\/      ";
+	
+	private static String convertToString(String[] number){
+		StringBuilder[] lines = new StringBuilder[6];
+	      for (int i = 0; i < 6; i++) {
+	          lines[i] = new StringBuilder();
+	      }
+
+	      for (int i = 0; i < 6; i++) {
+              lines[i].append(String.format("%-" + 8 + "s", number[i])).append("  "); 
+          }
+
+	      StringBuilder result = new StringBuilder();
+	      for (StringBuilder line : lines) {
+	          result.append(line.toString()).append("\n");
+	      }
+
+	      return result.toString();
+	}
+	
+	
+    String[] letI = new String[]{
+        		    " _____  ",
+                "|_   _| ",
+                "  | |   ",
+                "  | |   ",
+                " _| |_  ",
+                "|_____| "
+        };
+    String letteraI = convertToString(letI);
+    
+    
+    String[] letV = new String[]{
+                "__      __",
+                "\\ \\    / /",
+                " \\ \\  / / ",
+                "  \\ \\/ /  ",
+                "   \\  /   ",
+                "    \\/    "
+        };
+    String letteraV = convertToString(letV);
+    
+    String[] letX = new String[]{
+                "__   __",
+                "\\ \\ / /",
+                " \\ V /  ",
+                "  > <   ",
+                " / · \\  ",
+                "/_/ \\_\\"
+        };
+    String letteraX = convertToString(letX);
+    
+    String[] letL = new String[]{
+                " _       ",
+        		    "| |      ",
+                "| |      ",
+                "| |      ",
+                "| |_ _   ",
+                "|______| "
+        };
+    String letteraL = convertToString(letL);
+    
+    String[] letC = new String[]{
+                " _____  ",
+        		    "/  ___| ",
+                "| |     ",
+                "| |     ",
+                "| |___  ",
+                "\\_____| "
+        };
+    String letteraC = convertToString(letC);
+    
+    String[] letD = new String[]{
+                " _____   ",
+        		    "|  __ \\  ",
+                "| |  | | ",
+                "| |  | | ",
+                "| |__| | ",
+                "|_____/  "
+        };
+    String letteraD = convertToString(letD);
+    
+    String[] letM = new String[]{
+                " __  __  ",
+        		    "|  \\/  | ",
+                "| \\  / | ",
+                "| |\\/| | ",
+                "| |  | | ",
+                "|_|  |_| "
+        };
+    String letteraM = convertToString(letM);
+    
+    String[] letXLIV = new String[]{
+        		"__   __    _          _____    __   __",   
+"\\ \\ / /   | |        |_   _|   \\ \\ / /   ",
+" \\ V /    | |          | |      \\ V /    ",
+"  > <     | |          | |       > <     ",
+" / · \\    | |_ _      _| |_     / · \\    ",
+"/_/ \\_\\   |______|   |_____|   /_/ \\_\\   "
+        };
+    String letteraXLIV = convertToString(letXLIV);
+    
+    String[] letXCIV = new String[]{
+                "__   __    _____     _____    __      __",  
+"\\ \\ / /   /  ___|   |_   _|   \\ \\    / /",
+" \\ V /    | |         | |      \\ \\  / /  ",
+"  > <     | |         | |       \\ \\/ /    ",
+" / · \\    | |___     _| |_       \\  /     ",
+"/_/ \\_\\   \\_____|   |_____|       \\/      "
+        };
+    String letteraXCIV = convertToString(letXCIV);
+    
+    String[] letCXXIII = new String[]{
+                " _____    __   __   __   __    _____     _____     _____",    
+"/  ___|   \\ \\ / /   \\ \\ / /   |_   _|   |_   _|   |_   _|  ",  
+"| |        \\ V /     \\ V /      | |       | |       | |     ",
+"| |         > <       > <       | |       | |       | |     ",
+"| |___     / · \\     / · \\     _| |_     _| |_     _| |_    ",
+"\\_____|   /_/ \\_\\   /_/ \\_\\   |_____|   |_____|   |_____|   "
+        };
+    String letteraCXXIII = convertToString(letCXXIII);
+    
+    String[] letCCLIX = new String[]{
+                " _____     _____     _          _____    __   __ ",  
+"/  ___|   /  ___|   | |        |_   _|   \\ \\ / /   ",
+"| |       | |       | |          | |      \\ V /    ",
+"| |       | |       | |          | |       > <     ",
+"| |___    | |___    | |_ _      _| |_     / · \\    ",
+"\\_____|   \\_____|   |______|   |_____|   /_/ \\_\\   "
+        };
+    String letteraCCLIX = convertToString(letCCLIX);
+    
+    String[] letCDLXVIII = new String[]{
+                " _____     _____      _         __   __   __      __   _____     _____     _____ ",   
+"/  ___|   |  __ \\    | |        \\ \\ / /   \\ \\    / /  |_   _|   |_   _|   |_   _|   ",
+"| |       | |  | |   | |         \\ V /     \\ \\  / /     | |       | |       | |     ",
+"| |       | |  | |   | |          > <       \\ \\/ /      | |       | |       | |     ",
+"| |___    | |__| |   | |_ _      / · \\       \\  /      _| |_     _| |_     _| |_    ",
+"\\_____|   |_____/    |______|   /_/ \\_\\       \\/      |_____|   |_____|   |_____|   "
+        };
+    String letteraCDLXVIII = convertToString(letCDLXVIII);
+    
+    String[] letDLXXXVII = new String[]{
+                " _____      _         __   __   __   __   __   __   __      __   _____     _____ ",   
+"|  __ \\    | |        \\ \\ / /   \\ \\ / /   \\ \\ / /   \\ \\    / /  |_   _|   |_   _|   ",
+"| |  | |   | |         \\ V /     \\ V /     \\ V /     \\ \\  / /     | |       | |     ",
+"| |  | |   | |          > <       > <       > <       \\ \\/ /      | |       | |     ",
+"| |__| |   | |_ _      / · \\     / · \\     / · \\       \\  /      _| |_     _| |_    ",
+"|_____/    |______|   /_/ \\_\\   /_/ \\_\\   /_/ \\_\\       \\/      |_____|   |_____|   "
+        };
+    String letteraDLXXXVII = convertToString(letDLXXXVII);
+    
+    String[] letDCCXXXVI = new String[]{
+                " _____      _____     _____    __   __   __   __   __   __   __      __   _____ ",   
+"|  __ \\    /  ___|   /  ___|   \\ \\ / /   \\ \\ / /   \\ \\ / /   \\ \\    / /  |_   _|   ",
+"| |  | |   | |       | |        \\ V /     \\ V /     \\ V /     \\ \\  / /     | |   ",  
+"| |  | |   | |       | |         > <       > <       > <       \\ \\/ /      | |     ",
+"| |__| |   | |___    | |___     / · \\     / · \\     / · \\       \\  /      _| |_    ",
+"|_____/    \\_____|   \\_____|   /_/ \\_\\   /_/ \\_\\   /_/ \\_\\       \\/      |_____|   "
+        };
+    String letteraDCCXXXVI = convertToString(letDCCXXXVI);
+    
+    String[] letCMLIV = new String[]{
+                " _____     __  __     _          _____    __      __ ",  
+"/  ___|   |  \\/  |   | |        |_   _|   \\ \\    / /  ",
+"| |       | \\  / |   | |          | |      \\ \\  / /   ",
+"| |       | |\\/| |   | |          | |       \\ \\/ /    ",
+"| |___    | |  | |   | |_ _      _| |_       \\  /     ",
+"\\_____|   |_|  |_|   |______|   |_____|       \\/      "
+        };
+    String letteraCMLIV = convertToString(letCMLIV);
 
 
     
@@ -56,4 +214,5 @@ public class RomanPrinterTest {
         RomanPrinter.print(0);
     }
 }
+
 
